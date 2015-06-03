1466a0f953fd95bc49221f926e26aa0e41fa1cd6
shimizu_yasuhiro <shimizu_yasuhiro@cyberagent.co.jp>
2015-06-03 20:12:23 +0900

    :green_check_mark: test for RealmObservable#results

--- rxrealm/src/androidTest/java/net/yslibrary/rxrealm/RealmObservableTest.java
+++ rxrealm/src/androidTest/java/net/yslibrary/rxrealm/RealmObservableTest.java
@@ -6,7 +6,6 @@ import net.yslibrary.rxrealm.entity.Hobby;
 import net.yslibrary.rxrealm.entity.Person;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -18,6 +17,7 @@ import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmList;
+import io.realm.RealmResults;
 import rx.Observable;
 import rx.functions.Func1;
 
@@ -154,9 +154,80 @@ public class RealmObservableTest {
     }
 
     @Test
-    @Ignore("TBD")
     public void RealmObservable_results() {
+        Context context = InstrumentationRegistry.getTargetContext();
 
+        // setup
+        RealmObservable.object(context, REALM_NAME, new Func1<Realm, Person>() {
+            @Override
+            public Person call(Realm realm) {
+                Person person = realm.createObject(Person.class);
+                person.setFirstName("John");
+                person.setLastName("Smith");
+
+                RealmList<Hobby> hobbies = new RealmList<Hobby>();
+                Hobby hobby1 = realm.createObject(Hobby.class);
+                hobby1.setName("tv game");
+
+                hobbies.add(hobby1);
+
+                Hobby hobby2 = realm.createObject(Hobby.class);
+                hobby2.setName("cycling");
+
+                hobbies.add(hobby2);
+
+                person.setHobbies(hobbies);
+
+                return person;
+            }
+        }).map(new Func1<Person, PersonDto>() {
+            @Override
+            public PersonDto call(Person person) {
+                List<HobbyDto> hobbies = Observable.from(person.getHobbies())
+                        .map(new Func1<Hobby, HobbyDto>() {
+                            @Override
+                            public HobbyDto call(Hobby hobby) {
+                                return new HobbyDto(hobby.getName());
+                            }
+                        }).toList().toBlocking().single();
+
+                return new PersonDto(person.getFirstName(), person.getLastName(), hobbies);
+            }
+        }).toBlocking().first();
+
+        List<PersonDto> people = RealmObservable.results(context, REALM_NAME,
+                new Func1<Realm, RealmResults<Person>>() {
+                    @Override
+                    public RealmResults<Person> call(Realm realm) {
+                        return realm.where(Person.class).findAll();
+                    }
+                }).map(new Func1<RealmResults<Person>, List<PersonDto>>() {
+            @Override
+            public List<PersonDto> call(RealmResults<Person> persons) {
+                return Observable.from(persons)
+                        .map(new Func1<Person, PersonDto>() {
+                            @Override
+                            public PersonDto call(Person person) {
+                                List<HobbyDto> hobbies = Observable.from(person.getHobbies())
+                                        .map(new Func1<Hobby, HobbyDto>() {
+                                            @Override
+                                            public HobbyDto call(Hobby hobby) {
+                                                return new HobbyDto(hobby.getName());
+                                            }
+                                        }).toList().toBlocking().first();
+                                return new PersonDto(person.getFirstName(), person.getLastName(),
+                                        hobbies);
+                            }
+                        }).toList().toBlocking().first();
+            }
+        }).toBlocking().first();
+
+        assertThat(people).hasSize(1);
+        assertThat(people.get(0).getFirstName()).isEqualTo("John");
+        assertThat(people.get(0).getLastName()).isEqualTo("Smith");
+        assertThat(people.get(0).getHobbies()).hasSize(2);
+        assertThat(people.get(0).getHobbies().get(0).getName()).isEqualTo("tv game");
+        assertThat(people.get(0).getHobbies().get(1).getName()).isEqualTo("cycling");
     }
 
 }
